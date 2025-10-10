c89303bcc8bdaaa9dba66a748cf186e20be30b5e
vannnt23411 <vannnt23411@st.uel.edu.vn>
2025-10-10 10:13:13 +0700

    Add files via upload

---
+++ learn_js/js/main.js
@@ -0,0 +1,20 @@
+function first_degree(a,b){
+    if (a==0 && b==0)
+        return "INF";
+    if (a==0 && b!=0)
+        return "NO";
+    return Math.round((-b / a) * 100) / 100;
+}
+function call_first_degree(a,b,result1, result2)
+{
+    r=first_degree(a,b)
+    result1.value=r
+    result2.innerText=r
+}
+function is_leap_year(year)
+{
+    if((year%4==0 && year%100!=0) || (year%400==0))
+        return true
+    else
+        return false
+}
