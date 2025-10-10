f2cdfbb04410a301b4353ccfcc5b05efc5cec619
vannnt23411 <vannnt23411@st.uel.edu.vn>
2025-10-10 12:22:07 +0700

    Add files via upload

---
+++ ex54/mainjs.js
@@ -0,0 +1,64 @@
+function findMax(a, b, c) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    c = parseFloat(c);
+    return Math.max(a, b, c);
+}
+
+function findMin(a, b, c) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    c = parseFloat(c);
+    return Math.min(a, b, c);
+}
+
+function calcSin(a) {
+    a = parseFloat(a);
+    return Math.sin(a * Math.PI / 180).toFixed(4); 
+}
+
+function calcCos(a) {
+    a = parseFloat(a);
+    return Math.cos(a * Math.PI / 180).toFixed(4); 
+}
+
+function showResult(value, resultBox) {
+    resultBox.value = value;
+}
+
+function isNumber(value)
+{
+    return !isNaN(value) && value.trim() !=="";
+}
+
+function handleOperation(op)
+{
+    const a=document.getElementById("a").value
+    const b=document.getElementById("b").value
+    const c=document.getElementById("c").value
+    const result=document.getElementById("result").value;
+    if(!isNumber(a) ||(!isNumber(b) && op !=='sin' && op!=='cos') || (!isNumber(c) && op !== 'sin' && op !== 'cos'))
+    {
+        alert("Bạn cần nhập 1 số!");
+        result.value="";
+        return;
+    }
+
+    let r;
+    switch (op)
+    {
+        case "max":
+            r=findMax(a,b,c);
+            break;
+        case "min":
+            r = findMin(a, b, c);
+            break;
+        case "sin":
+            r = calcSin(a);
+            break;
+        case "cos":
+            r = calcCos(a);
+            break;
+    }
+    showResult(r,result);
+}
